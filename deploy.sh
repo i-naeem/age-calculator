f11cbe9b455c9a4205c554bd5b6521d481538ee0
i-naeem <inaeem@protonmail.com>
2022-05-19 13:00:50 +0500

    :rocket: Setup deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ cd dist
 git init
 git checkout -b main
 git add -A
-git commit -m 'deploy'
+git commit -m ':rocket: deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
