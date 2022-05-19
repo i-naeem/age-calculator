13d7365d58d2192f2d4ce048e0c40b1939df5cab
i-naeem <inaeem@protonmail.com>
2022-05-19 13:31:09 +0500

    :bug: fix git url

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git commit -m ':rocket: deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.io:i-naeem/age-calculator.git main:gh-pages
+git push -f git@github.cpm:i-naeem/age-calculator.git main:gh-pages
 
 cd -
