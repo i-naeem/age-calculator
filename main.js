83e3d21f897bc81c0cf44bf67f852ee04398b951
i-naeem <inaeem@protonmail.com>
2022-07-03 17:06:11 +0500

    :sparkles: Set ageAtDate `date-selector` intial date to current date

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import getDateOfBirth from './utils/getDateOfBirth';
-import getDaysOptions from './utils/getDaysOptions';
-import populateDaysSelectElement from './utils/populateDaysSelectedElement';
+import "bootstrap/dist/css/bootstrap.min.css";
+import getDateOfBirth from "./utils/getDateOfBirth";
+import getDaysOptions from "./utils/getDaysOptions";
+import populateDaysSelectElement from "./utils/populateDaysSelectedElement";
 
-const dateOfBirthYear = document.getElementById('dob-year');
-const dateOfBirthMonth = document.getElementById('dob-month');
-const dateOfBirthDay = document.getElementById('dob-day');
+const dateOfBirthYear = document.getElementById("dob-year");
+const dateOfBirthMonth = document.getElementById("dob-month");
+const dateOfBirthDay = document.getElementById("dob-day");
 
-const ageAtYear = document.getElementById('age-at-year');
-const ageAtMonth = document.getElementById('age-at-month');
-const ageAtDay = document.getElementById('age-at-day');
+const ageAtYear = document.getElementById("age-at-year");
+const ageAtMonth = document.getElementById("age-at-month");
+const ageAtDay = document.getElementById("age-at-day");
 
-const alertElement = document.getElementById('alert');
-const form = document.getElementById('dob-form');
-const ageAlert = document.getElementById('age-alert');
-const ageTable = document.getElementById('age-table');
+const alertElement = document.getElementById("alert");
+const form = document.getElementById("dob-form");
+const ageAlert = document.getElementById("age-alert");
+const ageTable = document.getElementById("age-table");
 
 // Set the initial value for days
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const options = getDaysOptions(dateOfBirthYear.value, dateOfBirthMonth.value);
-  populateDaysSelectElement(dateOfBirthDay, options, '1');
+  populateDaysSelectElement(dateOfBirthDay, options, "1");
 
   // Load LocalStorage
-  dateOfBirthDay.value = localStorage.getItem('dobDay') || 8;
-  dateOfBirthMonth.value = localStorage.getItem('dobMonth') || 8;
-  dateOfBirthYear.value = localStorage.getItem('dobYear') || 2002;
+  dateOfBirthDay.value = localStorage.getItem("dobDay") || 8;
+  dateOfBirthMonth.value = localStorage.getItem("dobMonth") || 8;
+  dateOfBirthYear.value = localStorage.getItem("dobYear") || 2002;
 
   let today = new Date();
-  ageAtDay.value = localStorage.getItem('ageAtDay') || today.getDate();
-  ageAtMonth.value = localStorage.getItem('ageAtMonth') || today.getMonth();
-  ageAtYear.value = localStorage.getItem('ageAtYear') || today.getFullYear();
+  ageAtDay.value = localStorage.getItem("ageAtDay") || today.getDate();
+  ageAtMonth.value = localStorage.getItem("ageAtMonth") || today.getMonth();
+  ageAtYear.value = localStorage.getItem("ageAtYear") || today.getFullYear();
 });
 
 let previousSelectedDay = dateOfBirthDay.value;
@@ -51,9 +51,10 @@ dateOfBirthMonth.onchange = () => {
 // Age at Section
 
 // Set the initial value for days
-document.addEventListener('DOMContentLoaded', () => {
+let todayDate = new Date().getDate().toString();
+document.addEventListener("DOMContentLoaded", () => {
   const options = getDaysOptions(ageAtYear.value, ageAtMonth.value);
-  populateDaysSelectElement(ageAtDay, options);
+  populateDaysSelectElement(ageAtDay, options, todayDate);
 });
 
 let previousSelectedAgeAtDay = ageAtDay.value;
@@ -72,14 +73,14 @@ ageAtMonth.onchange = () => {
   populateDaysSelectElement(ageAtDay, options, previousSelectedAgeAtDay);
 };
 
-const yearsOfDateOfBirth = document.getElementById('years');
-const monthsOfDateOfBirth = document.getElementById('months');
-const weeksOfDateOfBirth = document.getElementById('weeks');
-const daysOfDateOfBirth = document.getElementById('days');
-const hoursOfDateOfBirth = document.getElementById('hours');
-const minutesOfDateOfBirth = document.getElementById('minutes');
-const secondsOfDateOfBirth = document.getElementById('seconds');
-const millisecondsOfDateOfBirth = document.getElementById('milliseconds');
+const yearsOfDateOfBirth = document.getElementById("years");
+const monthsOfDateOfBirth = document.getElementById("months");
+const weeksOfDateOfBirth = document.getElementById("weeks");
+const daysOfDateOfBirth = document.getElementById("days");
+const hoursOfDateOfBirth = document.getElementById("hours");
+const minutesOfDateOfBirth = document.getElementById("minutes");
+const secondsOfDateOfBirth = document.getElementById("seconds");
+const millisecondsOfDateOfBirth = document.getElementById("milliseconds");
 
 form.onsubmit = (event) => {
   event.preventDefault();
@@ -98,12 +99,12 @@ form.onsubmit = (event) => {
   } else {
     alertElement.hidden = true;
 
-    localStorage.setItem('ageAtDay', ageAtDay.value);
-    localStorage.setItem('ageAtMonth', ageAtMonth.value);
-    localStorage.setItem('ageAtYear', ageAtYear.value);
-    localStorage.setItem('dobDay', dateOfBirthDay.value);
-    localStorage.setItem('dobMonth', dateOfBirthMonth.value);
-    localStorage.setItem('dobYear', dateOfBirthYear.value);
+    localStorage.setItem("ageAtDay", ageAtDay.value);
+    localStorage.setItem("ageAtMonth", ageAtMonth.value);
+    localStorage.setItem("ageAtYear", ageAtYear.value);
+    localStorage.setItem("dobDay", dateOfBirthDay.value);
+    localStorage.setItem("dobMonth", dateOfBirthMonth.value);
+    localStorage.setItem("dobYear", dateOfBirthYear.value);
     const dob = getDateOfBirth(from, to);
 
     ageAlert.innerHTML = `<h1>${dob.dob}</h1>`;
